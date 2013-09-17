a921908e1d61095dbc13ae80f0f3edb11907a9fc
Rob Levy <robert.g.levy@gmail.com>
2013-09-17 18:39:58 +0100

    Added labels to nodes
    Also refactored _new_nodes to take the anonymous functions out and turn
    them into actual functions.
    
    Transition on nodes now affects stroke width.

--- gdm_d3_animation_library.js
+++ gdm_d3_animation_library.js
@@ -1,3 +1,9 @@
+// Global Constants
+var __transition_duration__ = 1500;
+var __max_node_size__ = 80;
+var __max_link_width__ = 15;
+var __max_node_stroke_width__ = 8;
+
 function set_default(x, default_val) {
   return typeof(x) === "undefined" ? default_val : x;
 }
--- label_maker.js
+++ label_maker.js
@@ -1,12 +1,36 @@
 function _show_label(d) {
-  if (d.hasOwnProperty("label")) {
-    var label = d.label;
-    var the_event = d3.event;
-    console.log (label);
+  var transition_duration = 500;
+  if (d.id == d.parent & d.hasOwnProperty("label")) {
+    if (d3.select(this).selectAll("text").empty()) {
+      var label = d3.select(this).append("text");
+      label
+        .attr("text-anchor", "middle")
+        .text(d.label)
+        .style("font-size","0px");
+        
+      var t_grow = label.transition()
+        .duration(transition_duration)
+        .style("font-size",calculate_text_size);
+       //.style("transform",function(d) {return "scale(" + calculate_text_size(d) + ")"});
+    }
   }
 }
 
 
 function add_labels() {
-  d3.selectAll(".node").on("mouseover", _show_label);
+  d3.selectAll("g.node").each(_show_label);
+}
+
+function calculate_text_size(d) {
+  var size_rescale = 5.0;
+  return Math.sqrt(d.size) * size_rescale + "px";
+}
+
+// Hat tip to a question on Stack Overflow for this.
+//http://stackoverflow.com/questions/10144934/transition-font-size-on-chrome-after-zoom-in
+function _font_size_tween(d) {
+  return d3.interpolate(
+    this.style.getPropertyValue("font-size"),
+    calculate_text_size(d)
+  );
 }
